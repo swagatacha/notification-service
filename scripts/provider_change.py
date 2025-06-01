3ddb0613f2c0c9c4a117919d209777e3867608bf
swagatacha <swagata.pisces@gmail.com>
2025-06-01 23:53:11 +0530

    code merge from master

--- scripts/provider_change.py
+++ scripts/provider_change.py
@@ -1,16 +1,12 @@
-import json
-from commons import config
 from dal.sql.sql_dal import NoSQLDal
 from datetime import datetime, timedelta
-
-from schemas.v1.service_provider import DalProviderDetail
 
 class ProviderChange:
     def __init__(self):
         self.__dal = NoSQLDal()
+        self.evaluate_and_switch_provider()
         self.SUCCESS_STATUS = ["DELIVERED_TO_HANDSET","DELIVRD"]
         self.THRESOLD_RATE = 90
-        self.evaluate_and_switch_provider()
 
     def evaluate_and_switch_provider(self):
         one_hour_ago = datetime.utcnow() - timedelta(hours=1)
@@ -29,23 +25,6 @@ class ProviderChange:
         print(f"Total: {total}, Successful: {successful}, Success rate: {success_rate:.2f}%")
         if success_rate < self.THRESOLD_RATE:
             providers = self.__dal.get_provider_info()
-            
-            if isinstance(providers, list) and len(providers) == 0:
-                providers = json.loads(config.SMS_PROVIDERS)
-                sorted_providers = sorted(providers, key=lambda x: x["prority"])
-                for provider in sorted_providers:
-                    try:
-                        data_dict = {
-                            "name" : provider['name'],
-                            "isActive": True if provider['priority'] == 1 else False,
-                            "createdBy": 10001,
-                            "CreatedAt" : datetime.utcnow().isoformat()
-                        }
-                        provider_obj = DalProviderDetail(**data_dict)
-                        self.__dal.service_provider_add(provider_obj)
-                    except Exception as e:
-                        print(f'{provider["name"]} this provider is not added due to {str(e)}')
-
             alternative_providers = [p for p in providers if not p['isActive']]
             new_provider = alternative_providers[0] if alternative_providers else None
 
